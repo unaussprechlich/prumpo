d66569c0fde113f8bba5ee83930630c92bdc1e6b
unaussprechlich@pc <keck.alexander@web.de>
2017-11-09 21:00:30 +0100

    M3 Screenshots/Entwurf

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null)
+                        .show();
             }
         });
     }
