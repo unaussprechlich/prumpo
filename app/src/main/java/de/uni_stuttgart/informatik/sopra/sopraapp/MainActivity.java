885f1e91de8701b372eb834f08b2ab7304255e4f
unaussprechlich@pc <keck.alexander@web.de>
2017-11-20 20:57:50 +0100

    M4 Setup

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -16,14 +16,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show());
     }
 
     @Override
