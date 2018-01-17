b990a9224b67bc86e763fe701aa35efbf3f435af
Elias Müller <st108477@stud.uni-stuttgart.de>
2018-01-17 14:35:40 +0100

    reworked activity restarting

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/feature/authentication/UserManager.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/feature/authentication/UserManager.java
@@ -1,16 +1,16 @@
 package de.uni_stuttgart.informatik.sopra.sopraapp.feature.authentication;
 
 import android.arch.lifecycle.LiveData;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-
+import de.uni_stuttgart.informatik.sopra.sopraapp.app.SopraApp;
+import de.uni_stuttgart.informatik.sopra.sopraapp.database.models.user.User;
+import de.uni_stuttgart.informatik.sopra.sopraapp.dependencyinjection.scopes.ApplicationScope;
 import org.greenrobot.eventbus.EventBus;
 
 import javax.inject.Inject;
-
-import de.uni_stuttgart.informatik.sopra.sopraapp.app.SopraApp;
-import de.uni_stuttgart.informatik.sopra.sopraapp.dependencyinjection.scopes.ApplicationScope;
-import de.uni_stuttgart.informatik.sopra.sopraapp.database.models.user.User;
 
 
 @ApplicationScope
@@ -42,16 +42,13 @@ public class UserManager {
         EventBus.getDefault().postSticky(new EventsAuthentication.Logout(currentUser.getValue()));
         this.currentUser = null;
 
-        //TODO testing only ########################################################################
-        //DANGER!
-        //SHIT!
-        //DON'T DO THIZZZZ
-        //This is how you clean all activities and all the other junk ...
-        //trust me i found this on stackoverflow .. this can't be wrong!!111!111!!
+        // restart app with first activity as default main activity
+        PackageManager packageManager = context.getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage(context.getPackageName());
+        ComponentName componentName = intent.getComponent();
+        Intent mainIntent = Intent.makeRestartActivityTask(componentName);
+        context.startActivity(mainIntent);
         System.exit(0);
-        //TODO SHIT ################################################################################
-
-        startAuthenticationActivity();
     }
 
     public LiveData<User> getCurrentUserAsLiveData() throws NoUserException {
