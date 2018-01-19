63c3f434154b72e73f9681c2c01a8d208b5fa7f3
Elias Müller <st108477@stud.uni-stuttgart.de>
2018-01-19 19:06:20 +0100

    About fragment filled with content

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/feature/sidebar/about/AboutFragment.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/feature/sidebar/about/AboutFragment.java
@@ -1,6 +1,8 @@
 package de.uni_stuttgart.informatik.sopra.sopraapp.feature.sidebar.about;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.uni_stuttgart.informatik.sopra.sopraapp.R;
 import de.uni_stuttgart.informatik.sopra.sopraapp.feature.sidebar.FragmentBackPressed;
 
@@ -18,7 +21,8 @@ public class AboutFragment extends AboutBindings implements FragmentBackPressed 
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_main_fragment_about, container, false);
+        View view = inflater.inflate(R.layout.activity_main_fragment_about,
+                container, false);
         ButterKnife.bind(this, view);
 
         return view;
@@ -29,4 +33,21 @@ public class AboutFragment extends AboutBindings implements FragmentBackPressed 
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle(strAppbarTitle);
     }
+
+    @OnClick(R.id.about_frag_container_contacts)
+    public void onContainerContactsPressed() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(MAIL_TO + strContactEmailAddress));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getEmailSubject());
+
+        startActivity(Intent.createChooser(emailIntent, strContactIntentChooserTitle));
+    }
+
+    @OnClick(R.id.about_frag_container_licence)
+    public void onContainerLicencePressed() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(strLicenceUrl));
+        startActivity(browserIntent);
+    }
+
+
+
 }
